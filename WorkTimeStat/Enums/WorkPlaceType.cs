661a8f87869b4803e5dbc23df32022a6ec9d91c6
Zsolt Gajdács <33376024+ZsoltGajdacs@users.noreply.github.com>
2021-03-02 18:51:46 +0100

    Added new placetype

--- WorkTimeStat/Enums/WorkPlaceType.cs
+++ WorkTimeStat/Enums/WorkPlaceType.cs
@@ -14,6 +14,9 @@ namespace WorkTimeStat.Enums
         [Description("Ügyfél telephelyén töltött munkanap")]
         [Display(Name = "Ügyfél telephelyén")]
         CUSTOMER,
+        [Description("Nap folyamán több különböző helyen történt munkavégzés")]
+        [Display(Name = "Kevert")]
+        Mixed,
         [Description("Máshol történt munkavégzés")]
         [Display(Name = "Máshol")]
         OTHER
