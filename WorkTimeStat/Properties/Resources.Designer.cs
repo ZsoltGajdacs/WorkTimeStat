6b7972b6a3150b0fda1b02e8655c61df93a017bf
Zsolt Gajdács <33376024+ZsoltGajdacs@users.noreply.github.com>
2023-01-06 11:06:29 +0100

    Place name clarification

--- WorkTimeStat/Properties/Resources.Designer.cs
+++ WorkTimeStat/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace WorkTimeStat.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -277,7 +277,7 @@ namespace WorkTimeStat.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Various.
+        ///   Looks up a localized string similar to Mixed.
         /// </summary>
         public static string enum_mixed_display {
             get {
